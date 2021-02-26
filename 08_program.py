db365d746764fe0096f4af39fd12ced855960d4e
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2021-02-26 11:14:27 +0530

    Create 08_program.py

---
+++ 08_program.py
@@ -0,0 +1,4 @@
+""" Write a program to create a server that listens to port 5009 using datagram sockets. Write a simple client program that requests the server 
+for a binary file. The server should service multiple clients concurrently and send the requested files in response."""
+
+# Start from here
