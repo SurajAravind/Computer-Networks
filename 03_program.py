936cb906b68aa8b4f43f27d565a4e1ff71f71cd8
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2021-02-26 10:53:34 +0530

    Create 03_program.py

---
+++ 03_program.py
@@ -0,0 +1,5 @@
+""" Write a program to create a server that listens to port 5003 using stream sockets. 
+Write a simple client program to connect to the server.  Send a simple text message "Hello" from the client to the server and the server to the client 
+and close the connection. """
+
+# Start from here
