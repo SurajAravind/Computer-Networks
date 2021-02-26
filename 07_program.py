5d7d7e47da263cc67c5b9148012e321fe230be24
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2021-02-26 11:04:25 +0530

    Create 07_program.py

---
+++ 07_program.py
@@ -0,0 +1,4 @@
+""" Write a program to create a server that listens to port 5007 using stream sockets. Write a simple client program to connect to the server. 
+Run multiple clients that request the server for text files. The server should service all clients concurrently. """
+
+# Start from here
