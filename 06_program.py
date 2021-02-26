b0553de5da924bdb69a4a96ace31cf40708cb21c
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2021-02-26 11:01:34 +0530

    Create 06_program.py

---
+++ 06_program.py
@@ -0,0 +1,4 @@
+""" Write a program to create a server that listens to port 5006 using stream sockets. Write a simple client program to connect to the server. Run multiple clients 
+that request the server for binary files. The server should service each client one after the other before terminating the connection. """
+
+# Start from here
