122034e34a857269eb2f4a4347cc92afbdbb81a0
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2021-02-26 10:57:13 +0530

    Create 05_program.py

---
+++ 05_program.py
@@ -0,0 +1,4 @@
+""" Write a program to create a server that listens to port 5005 using stream sockets. Write a simple client program to connect to the server. 
+The client should request for a text file and the server should return the file before terminating the connection. """
+
+# Start from here
