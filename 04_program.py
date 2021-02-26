d823944de33d1cd6d3349df734efd740955c2fc6
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2021-02-26 10:55:53 +0530

    Create 04_program.py

---
+++ 04_program.py
@@ -0,0 +1,5 @@
+""" Write a program to create a chat server that listens to port 5004 using stream sockets. Write
+a simple client program to connect to the server. Send multiple text messages from the client to the server and vice versa.
+When either party says "Bye" close the connection.  """
+
+# Start from here
